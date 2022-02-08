be23265bdf3dd9e8fd8fd8e1670fd938b8b4a781
JuanLP <juan.luis.palacios.p@gmail.com>
2022-02-08 11:51:17 -0500

    Remove remaining old code and add load method

--- js/main.js
+++ js/main.js
@@ -1,56 +1,4 @@
-let books = [
-  { title: 'Lorem', author: 'someone' },
-  { title: 'Lorem2', author: 'someone' },
-  { title: 'Lorem3', author: 'someone' },
-];
-
-function updateList() {
-  console.log(books);
-  document.getElementById('books-list').innerHTML = books.map(
-    (x) => `<li>
-             <h6>"${x.title}"  by ${x.author} </h6>
-             <button type="button">Remove</button>
-      </li>`,
-  ).join('');
-
-  const newBooks = JSON.stringify(books.map((x) => ({ ...x })));
-  localStorage.setItem('books', newBooks);
-
-  document.getElementById('books-list').querySelectorAll('button').forEach((btn, i) => {
-    btn.addEventListener('click', () => {
-      // here is the remove logic
-      books = books.filter((book, k) => i !== k);
-      updateList();
-    });
-  });
-}
-
-window.addEventListener('load', () => {
-  const newBooks2 = localStorage.getItem('books');
-  books = JSON.parse(newBooks2);
-  updateList();
-  const btnAdd = document.querySelector('.btnAdd');
-  btnAdd.addEventListener('click', (e) => {
-    e.preventDefault();
-    const title = form.title.value;
-    const author = form.author.value;
-    archive1.add(new Book(title, author));
-    books = archive1.books;
-
-  });
-
-  var archive1 = new BookList([
-    { title: "Book1", aythor: "author1" },
-    { title: "Book2", aythor: "author2" }]);
-
-  archive1.add("Harry Potter", "JK Rowling");
-  archive1.add("book 4", "Author 4");
-  books = archive1.books;
-
-  //console.log(archive1);
-});
-
-
+/* eslint-disable max-classes-per-file */
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -59,20 +7,43 @@ class Book {
 }
 
 class BookList {
-  constructor(books) {
-    this.books = books;
+  constructor() {
+    console.log(localStorage.getItem('books'));
+    this.books = [];
     this.htmlResult = '';
+    this.loaded = false;
+    window.addEventListener('load', () => {
+      this.loaded = true;
+      const btnAdd = document.querySelector('.btnAdd');
+      btnAdd.addEventListener('click', (e) => {
+        e.preventDefault();
+        const form = document.forms[0];
+        const title = form.title.value;
+        const author = form.author.value;
+        this.add(new Book(title, author));
+      });
+      this.updateList();
+    });
   }
 
   updateList() {
-    console.log("list update");
-    this.htmlResult = this.books.map((el) => {
-      return `<li>
+    if (!this.loaded) return;
+    this.htmlResult = this.books.map((el) => `<li>
              <h6>"${el.title}"  by ${el.author} </h6>
              <button type="button">Remove</button>
-      </li>`
-    }).join('');
+      </li>`).join('');
     document.getElementById('books-list').innerHTML = this.htmlResult;
+    document.getElementById('books-list').querySelectorAll('button').forEach((btn, i) => {
+      btn.addEventListener('click', () => {
+        this.remove(i);
+      });
+    });
+    localStorage.setItem('books', JSON.stringify(this.books.map((x) => ({ ...x }))));
+  }
+
+  load() {
+    this.books = JSON.parse(localStorage.getItem('books') || '[]').map((x) => new Book(x.title, x.author));
+    this.updateList();
   }
 
   add(x) {
@@ -82,8 +53,9 @@ class BookList {
 
   remove(i) {
     this.books.splice(i, 1);
+    this.updateList();
   }
 }
 
-
-
+const list = new BookList();
+list.load();
